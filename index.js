e5e4460397572c12c11845d0c8853f53aa1b84fc
Aryan Koul <aryankoul10@gmail.com>
2018-10-21 01:47:19 +0530

    Routes added

--- index.js
+++ index.js
@@ -15,6 +15,58 @@ const router = express.Router();
 
 
 
+router.get('/', (req, res) => {
+    res.json({sucess: true});
+});
+
+
+router.post('/login',(req, res) => {
+    const user  = req.body.email;
+    const pass = req.body.pass;
+
+    User.findOne({'email': user}, (err, user) => {
+        if (err) res.json({success: false,err: "server error"});
+
+        else if (user == null) res.json({success: false,err: "user not found"});
+        else {
+            if (user.pass === pass) {
+                let sites = [];
+                user.details.forEach((site, i) => sites.push(site.website));
+                res.json({success: true, sites: sites});
+            }
+            else res.json({success: false,err: "wrong password"});
+        }
+    });
+});
+
+router.post('/user',(req, res) => {
+
+    User.findOne({'email': req.body.email},(err, user) => {
+        if (err) res.json({success: false,err: "server error"});
+
+        if (user == null) res.json({success: false,err: "user not found"});
+        else {
+            if (user.pass === pass) {
+                let sites = [];
+                user.details.forEach((site, i) => sites.push(site.website));
+                res.json({success: true, sites: sites});
+            }
+            else res.json({success: false,err: "wrong password"});
+        }
+
+    } );
+});
+
+router.put('/user',(req,res) => {
+    const email = req.body.email;
+    const detail = {'website': req.body.website,'username': req.body.username, 'pass': req.body.pass};
+
+    User.updateOne({'email': email}, {$push : { details: detail}}, (err, res) => {
+
+    });
+
+
+});
 
 mongoose.connect("mongodb://aryan:aryan1@ds031541.mlab.com:31541/rovault", { useNewUrlParser: true,useCreateIndex: true } ,(err) =>{
     if(err) console.log("error occured");
